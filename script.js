5d5bfaf4cd6490d11046ab84e5bfc450a321bc2b
sitekacper <kacper19961996@gmail.com>
2024-11-03 14:16:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     console.log("JavaScript jest poprawnie podłączony i gotowy do użycia.");
-});
 
+    // Funkcja przykładowa, dodająca alert po kliknięciu nagłówka
+    const header = document.querySelector("header h1");
+    header.addEventListener("click", function() {
+        alert("Witaj na mojej stronie portfolio!");
+    });
+});
